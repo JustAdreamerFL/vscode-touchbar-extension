72565081a6d5456b7d0a3b8856801385dd727dfe
JustAdreamerFL <Slavouli@gmail.com>
2024-05-04 14:51:27 +0200

    Now I can publish it via terminal Added one TODO

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
+//TODO: Add rename shortcut
 export function activate(context: vscode.ExtensionContext) {
 	///// MOVING IN UI
 	/// Activate flutter build
